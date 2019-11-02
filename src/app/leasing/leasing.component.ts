0d778d95277f1e88bed47007ae61025b06405b13
Naveen <naveensnewid@gmail.com>
2019-11-02 14:01:10 +0530

    Leasing folder file upload fixed

--- src/app/leasing/leasing.component.ts
+++ src/app/leasing/leasing.component.ts
@@ -1,6 +1,7 @@
 import { PageService } from './../core/services/page.service';
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-leasing',
@@ -12,28 +13,32 @@ export class LeasingComponent implements OnInit {
   banner = '';
   pageTitle = '';
   currentJustify = 'fill';
-  private leasingForm: any;
+  leasingForm: FormGroup;
   leasingTypes = ['Store', 'Kiosk', 'Promotional space'];
+  submitted = false;
+  responseStatus = false;
+  responseText = '';
 
 
   constructor(
     private pageService: PageService,
+    private formBuilder: FormBuilder,
     private cd: ChangeDetectorRef
     ) { }
 
   ngOnInit() {
 
-    this.leasingForm = new FormGroup({
-      leasingType: new FormControl(null, Validators.required),
-      brandName: new FormControl(null, Validators.required),
-      franchise: new FormControl(null, Validators.required),
-      countryOfOrigin: new FormControl(null),
-      companyName: new FormControl(null, Validators.required),
-      address: new FormControl(null),
-      website: new FormControl(null),
-      name: new FormControl(null, Validators.required),
-      mobileNumber: new FormControl(null, Validators.required),
-      attachment: new FormControl(null)
+    this.leasingForm = this.formBuilder.group({
+      type: ['', Validators.required],
+      brandName: ['', Validators.required],
+      franchise: ['', Validators.required],
+      country: [''],
+      companyName: ['', Validators.required],
+      address: [''],
+      website: [''],
+      name: ['', Validators.required],
+      mobileNumber: ['', Validators.required],
+      attachedProfile: [null]
   });
 
     this.pageService.getPageBanner('leasing', localStorage.getItem('lang'))
@@ -44,57 +49,50 @@ export class LeasingComponent implements OnInit {
   }
 
   onFileChange(event) {
-
-    /*
-    let reader = new FileReader();
-   
-    if(event.target.files && event.target.files.length) {
-      const [file] = event.target.files;
-      reader.readAsDataURL(file);
-    
-      reader.onload = () => {
-        this.leasingForm.patchValue({
-          attachment: reader.result
-        });        
-        
-        this.cd.markForCheck();
-      };
-    }
-    */
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      this.leasingForm.get('attachment').setValue(file);
-    }
+    const file = (event.target as HTMLInputElement).files[0];
+    this.leasingForm.patchValue({
+      attachedProfile: file
+    });
+    this.leasingForm.get('attachedProfile').updateValueAndValidity()
   }
 
   onSubmit()
   {
+    this.submitted = true;
     if(this.leasingForm.valid) {
-      const formData = new FormData();
-      formData.append('attachedProfile', this.leasingForm.get('attachment').value);
-      formData.append('type', this.leasingForm.get('leasingType').value);
-      formData.append('brandName', this.leasingForm.get('brandName'));
-      formData.append('franchise', this.leasingForm.get('franchise'));
-      formData.append('country', this.leasingForm.get('countryOfOrigin'));
-      formData.append('companyName', this.leasingForm.get('companyName'));
-      formData.append('address', this.leasingForm.get('address'));
-      formData.append('website', this.leasingForm.get('website'));
-      formData.append('name', this.leasingForm.get('name'));
-      formData.append('mobileNumber', this.leasingForm.get('mobileNumber'));
+      const formData: any = new FormData();         
+      formData.append('attachedProfile', this.leasingForm.get('attachedProfile').value);
+      formData.append('type', this.leasingForm.get('type').value);
+      formData.append('brandName', this.leasingForm.get('brandName').value);
+      formData.append('franchise', this.leasingForm.get('franchise').value);
+      formData.append('country', this.leasingForm.get('country').value);
+      formData.append('companyName', this.leasingForm.get('companyName').value);
+      formData.append('address', this.leasingForm.get('address').value);
+      formData.append('website', this.leasingForm.get('website').value);
+      formData.append('name', this.leasingForm.get('name').value);
+      formData.append('mobileNumber', this.leasingForm.get('mobileNumber').value);
+      // const formData = this.leasingForm.value;      
       this.pageService.postLeasingForm(formData).subscribe(
         response => {
-          console.log (response);
+          this.responseStatus = true;
+          this.responseText = response.message;
+          if(response.status === 200) {
+            this.revert();
+          }
         }
       );
     }
-    else 
-    {
-      console.log('Invalid Form');
+    else {
+      console.log("Invalid Form");
+      return;
     }
+ 
 
   }
+  get f() { return this.leasingForm.controls; }
 
   revert() {
+
     this.leasingForm.reset();
   }
 
